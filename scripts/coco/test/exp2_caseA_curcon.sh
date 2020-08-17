b975212a7c1c4ac4172a6cef10c77eab2bcaf0ab
Thuy Ng <thuypn9a4@gmail.com>
2020-08-17 13:53:35 +0700

    add coco-exp2-caseA-curcon

---
+++ scripts/coco/test/exp2_caseA_curcon.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+PROPOSAL=100
+IOU=0.5
+PAIRS=6
+CONFIG="configs/sscod/coco/exp2_caseA_curcon.py"
+WORK_DIR="/checkpoints/sscod/coco/exp2_caseA_curcon"
+
+GPUS=6
+CKPT="${WORK_DIR}/epoch_12.pth"
+PKL="${WORK_DIR}/epoch_12.pkl"
+
+python -m torch.distributed.launch \
+    --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) \
+    tools/test.py ${CONFIG} ${CKPT} \
+    --launcher pytorch --out ${PKL} --options work_dir=$WORK_DIR
+
+export EONC='1'
+echo "Evaluating EONC..."
+python tools/eval_sscod.py ${PKL} ${CONFIG} \
+    --num_proposal_pairs=$PROPOSAL --iou_thr=$IOU --k_pairs=$PAIRS
+
+export EONC='0'
+echo "Evaluating ONC..."
+python tools/eval_sscod.py ${PKL} ${CONFIG} \
+    --num_proposal_pairs=$PROPOSAL --iou_thr=$IOU --k_pairs=$PAIRS
